9501604e483629bea27b2809a55424ab11e434b5
KingWhales <olawaleafosi@gmail.com>
2023-10-03 16:51:00 +0000

    main.tf configuration

--- main.tf
+++ main.tf
@@ -10,3 +10,16 @@ terraform {
 provider "random" {
   # Configuration options
 }
+
+resource "random_string" "bucket_name" {
+  length = 16
+  special = true
+}
+
+output "random_bucket_name_id" {
+  value = random_string bucket_name.id
+}
+
+output "random_bucket_name_result" {
+  value = random_string bucket_name.result
+}
