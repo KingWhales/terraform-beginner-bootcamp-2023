c1cd0c2a98bcdb3c829873e94a9e832b5890d282
KingWhales <olawaleafosi@gmail.com>
2023-10-04 15:58:59 +0000

    #7 add terraform random provider

--- main.tf
+++ main.tf
@@ -16,10 +16,6 @@ resource "random_string" "bucket_name" {
   special = true
 }
 
-output "random_bucket_name_id" {
-  value = random_string bucket_name.id
-}
-
-output "random_bucket_name_result" {
-  value = random_string bucket_name.result
+output "random_bucket_name" {
+  value = random_string.bucket_name.result
 }
