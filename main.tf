583d634c425398480b63a66907050d5169a1d9dc
KingWhales <olawaleafosi@gmail.com>
2023-11-08 17:00:43 +0000

    .tf configuration

--- main.tf
+++ main.tf
@@ -1,35 +1,3 @@
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
-
-terraform {
-  cloud {
-    organization = "KingWhales"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-
-
-provider "random" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
   lower = true
   upper = false
@@ -39,8 +7,9 @@ resource "random_string" "bucket_name" {
 
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+
+  tags = {
+    UserUuid  = "My bucket"
+  }
 }
