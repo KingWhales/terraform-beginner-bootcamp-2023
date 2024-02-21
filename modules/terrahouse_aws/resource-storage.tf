62cd797b57bbbd29cebacaa50adb43d833d251de
KingWhales <olawaleafosi@gmail.com>
2024-02-21 11:10:43 +0000

    configuring....

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -35,6 +35,20 @@ resource "aws_s3_object" "index_html" {
   }
 }
 
+resource "aws_s3_object" "upload_assets" {
+  for_each = fileset("${path.root}/public/assets", "*")
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "assets/${each.key}"
+  source = "${path.root}/public/assets/${each.key}"
+  #content_type = "text/html"
+  etag = filemd5("${path.root}/public/assets/${each.key}")
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
+
+}
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
 resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
