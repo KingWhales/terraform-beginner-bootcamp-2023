925062dd784e4ac425e056d1dc8225a0464c7449
KingWhales <111932225+KingWhales@users.noreply.github.com>
2024-03-13 15:40:44 +0100

    38 terratowns provider (#40)
    * # 38 Configuring
    
    * #38 wip setting up the skeleton for creating a custom terraform provider
    
    * 338 wip setting up the skeleton for creating a custom terraform provider
    
    * #38 implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,25 +1,40 @@
 
 
-terraform {
-  cloud {
-    organization = "KingWhales"
-
-    workspaces {
-      name = "terra-house-1"
+ terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
     }
   }
-}
+ }
+#  terraform {
+   
+#   cloud {
+#     organization = "KingWhales"
+
+#     workspaces {
+#       name = "terra-house-1"
+#     }
+#   }
+# }
 
 
-provider "random" {
-  # Configuration options
+# provider "random" {
+#   # Configuration options
+# }
+
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
+  token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  error_html_filepath = "./public/error.html"
-  index_html_filepath = "./public/index.html"
-  content_version = var.content_version
-}
+#module "terrahouse_aws" {
+#  source = "./modules/terrahouse_aws"
+#  user_uuid = var.user_uuid
+#  bucket_name = var.bucket_name
+#  error_html_filepath = "./public/error.html"
+#  index_html_filepath = "./public/index.html"
+#  content_version = var.content_version
+#}
